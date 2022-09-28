c19b295689855b8a2ae6860b82e4ff3917e2e748
shannen yap <shannen.yml@gmail.com>
2022-09-28 13:14:03 -0700

    Added if statements for user input

--- calculator.py
+++ calculator.py
@@ -9,6 +9,15 @@ on = True
 while True:
     equation = input("What is the equation: ")
     equation = equation.split(' ')
+    num_1 = equation[1]
+    num_2 = equation[2]
+
+    if len(equation) < 2:
+        print("not enough input.")
+    
+    if num_1.isdigit() == False:
+        print("That is not a number, try again")
+
     if (type(equation[1]) != int) and (type(equation[2]) != int): 
         print("That is not an equation")
     if equation[0] == "+":
@@ -38,5 +47,6 @@ while True:
         answer = mod(float(equation[1]), float(equation[2]))
         print(answer)
     if equation[0] == "q":
+        print("Exit calculator")
         break
     
