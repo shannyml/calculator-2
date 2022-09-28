f09182b21da4c77ddc665d05ea058716da0b4d1a
shannen yap <shannen.yml@gmail.com>
2022-09-28 13:45:28 -0700

    updated code. provide hints if incorrect input

--- calculator.py
+++ calculator.py
@@ -9,44 +9,48 @@ on = True
 while True:
     equation = input("What is the equation: ")
     equation = equation.split(' ')
-    num_1 = equation[1]
-    num_2 = equation[2]
+    if equation[0] == "q":
+        break
 
     if len(equation) < 2:
-        print("not enough input.")
+        print("Not enough input")
     
-    if num_1.isdigit() == False:
-        print("That is not a number, try again")
+    else:
+        if len(equation) < 3:
+            num_1 = equation[1]
+            if not num_1.isdigit():
+                print("That is not a number")
+            else:
+                if equation[0] == "square":
+                    answer = square(float(equation[1]))
+                    print(answer)
+                if equation[0] == "cube":
+                    answer = cube(float(equation[1]))
+                    print(answer)
 
-    if (type(equation[1]) != int) and (type(equation[2]) != int): 
-        print("That is not an equation")
-    if equation[0] == "+":
-        answer = add(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[0] == "-":
-        answer = subtract(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[0] == "*":
-        answer = multiply(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[0] == "/":
-        answer = divide(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[1] != int:
-        print("That is not an equation")
-    if equation[0] == "square":
-        answer = square(float(equation[1]))
-        print(answer)
-    if equation[0] == "cube":
-        answer = cube(float(equation[1]))
-        print(answer)
-    if equation[0] == "pwr":
-        answer = power(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[0] == "mod":
-        answer = mod(float(equation[1]), float(equation[2]))
-        print(answer)
-    if equation[0] == "q":
-        print("Exit calculator")
-        break
-    
+        else:
+            if len(equation) > 2:
+                num_1 = equation[1]
+                num_2 = equation[2]
+                if not num_1.isdigit() or not num_2.isdigit():
+                    print("That is not a number")
+                else:
+                    if equation[0] == "+":
+                        answer = add(float(equation[1]), float(equation[2]))
+                        print(answer)
+                    if equation[0] == "-":
+                        answer = subtract(float(equation[1]), float(equation[2]))
+                        print(answer)
+                    if equation[0] == "*":
+                        answer = multiply(float(equation[1]), float(equation[2]))
+                        print(answer)
+                    if equation[0] == "/":
+                        answer = divide(float(equation[1]), float(equation[2]))
+                        print(answer)
+                    if equation[0] == "pwr":
+                        answer = power(float(equation[1]), float(equation[2]))
+                        print(answer)
+                    if equation[0] == "mod":
+                        answer = mod(float(equation[1]), float(equation[2]))
+                        print(answer)
+            
