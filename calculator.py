300de7a9f0a88e583ae3cf61afad3899f492b2ab
shannen yap <shannen.yml@gmail.com>
2022-09-28 09:38:09 -0700

    added while loop and if statement for calculator

--- calculator.py
+++ calculator.py
@@ -5,3 +5,25 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+on = True
+while True:
+    equation = input("What is the equation: ")
+    equation = equation.split(',')
+    if equation[0] == "+":
+        add(float(equation[1]), float(equation[2]))
+    if equation[0] == "-":
+        subtract(float(equation[1]), float(equation[2]))
+    if equation[0] == "*":
+        multiply(float(equation[1]), float(equation[2]))
+    if equation[0] == "/":
+        divide(float(equation[1]), float(equation[2]))
+    if equation[0] == "square":
+        square(float(equation[1]))
+    if equation[0] == "cube":
+        cube(float(equation[1]))
+    if equation[0] == "pwr":
+        power(float(equation[1]), float(equation[2]))
+    if equation[0] == "mod":
+        mod(float(equation[1]), float(equation[2]))
+    if equation[0]:
+        on = False
